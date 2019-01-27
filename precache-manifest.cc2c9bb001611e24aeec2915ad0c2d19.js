c36441d29d7a91bcc3c2d3ccb393bbe78fe697e1
Ezziu <arzetelski@gmail.com>
2019-01-27 15:22:24 +0100

    Updates

---
+++ precache-manifest.cc2c9bb001611e24aeec2915ad0c2d19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "607bd7681fde788eb844",
+    "url": "/smn2/static/js/runtime~main.607bd768.js"
+  },
+  {
+    "revision": "e8dc19958aea834d6df9",
+    "url": "/smn2/static/js/main.e8dc1995.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/smn2/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "e8dc19958aea834d6df9",
+    "url": "/smn2/static/css/main.5f919ce2.chunk.css"
+  },
+  {
+    "revision": "1a0214d4b5dc112f32b23654cfa8b694",
+    "url": "/smn2/index.html"
+  }
+];
